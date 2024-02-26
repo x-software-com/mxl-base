e6e201600adb4ed26499996bb69eefd1b3eed519
marcbull <31337222+marcbull@users.noreply.github.com>
2024-02-26 07:37:37 +0100

    feat: add third party licenses module

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ pub mod logging;
 pub mod misc;
 pub mod proc_dir;
 
+#[cfg(feature = "third_party_licenses")]
+pub mod third_party_licenses;
+
 pub use about::about;
 pub use misc::init;
 #[cfg(feature = "with_test")]
