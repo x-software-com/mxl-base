4cbe06577dc91f3ac449c4a17f20956a4acccb2e
acpiccolo <60399193+acpiccolo@users.noreply.github.com>
2024-03-07 10:50:40 +0100

    refactor: changed localization init()

--- src/localization.rs
+++ src/localization.rs
@@ -2,46 +2,45 @@ use i18n_embed::{
     fluent::{fluent_language_loader, FluentLanguageLoader},
     DefaultLocalizer, DesktopLanguageRequester, LanguageLoader, Localizer,
 };
-use once_cell::sync::Lazy;
+use once_cell::sync::OnceCell;
 use rust_embed::RustEmbed;
 
 #[derive(RustEmbed)]
 #[folder = "i18n/"]
 struct Localizations;
 
-pub(crate) static LANGUAGE_LOADER: Lazy<FluentLanguageLoader> = Lazy::new(|| {
-    let loader: FluentLanguageLoader = fluent_language_loader!();
+// A language loader must always needs to be initialized by an init function.
+// Do not use an automatic initialization implementation such as "once_cell::sync::Lacy",
+// this can lead to a deadlock!
+pub static LANGUAGE_LOADER: OnceCell<FluentLanguageLoader> = OnceCell::new();
 
+pub(crate) fn init() {
+    let loader = fluent_language_loader!();
     loader
         .load_fallback_language(&Localizations)
         .expect("Error while loading fallback language");
 
-    loader
-});
+    let localizer = DefaultLocalizer::new(&loader, &Localizations);
+    let requested_languages = DesktopLanguageRequester::requested_languages();
+    if let Err(error) = localizer.select(&requested_languages) {
+        log::error!("Error while loading language: {error}");
+    }
+    LANGUAGE_LOADER.set(loader).expect("Localization already initialized");
+}
+
+pub(crate) fn language_loader() -> &'static FluentLanguageLoader {
+    LANGUAGE_LOADER.get().expect("Localization is not initialized")
+}
 
 pub(crate) mod helper {
     macro_rules! fl {
     ($message_id:literal) => {{
-        i18n_embed_fl::fl!($crate::localization::LANGUAGE_LOADER, $message_id)
+        i18n_embed_fl::fl!($crate::localization::language_loader(), $message_id)
     }};
 
     ($message_id:literal, $($args:expr),*) => {{
-        i18n_embed_fl::fl!($crate::localization::LANGUAGE_LOADER, $message_id, $($args), *)
+        i18n_embed_fl::fl!($crate::localization::language_loader(), $message_id, $($args), *)
     }};
 }
     pub(crate) use fl;
-}
-
-// Get the `Localizer` to be used for localizing this library.
-pub(crate) fn localizer() -> Box<dyn Localizer> {
-    Box::new(DefaultLocalizer::new(&*LANGUAGE_LOADER, &Localizations))
-}
-
-pub(crate) fn init() {
-    let localizer = crate::localization::localizer();
-    let requested_languages = DesktopLanguageRequester::requested_languages();
-
-    if let Err(error) = localizer.select(&requested_languages) {
-        log::error!("Error while loading language: {error}");
-    }
 }
